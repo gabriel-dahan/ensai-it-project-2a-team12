08c7717b5667f82d5e35606940f05e42b476cd02
Gabriel D. <gabrieldahan05@gmail.com>
2026-09-04 16:23:11 +0200

    Added template db_connection/singleton class, updated pyproject.toml, updated class_diagram, updated .gitignore

--- api/src/business_object/user.py
+++ api/src/business_object/user.py
@@ -1,7 +1,23 @@
+import bcrypt
+
+from api.src.business_object.dju import DjuCalculation
+from api.src.business_object.geo_zone import Zoning
+
 
 class User:
 
     def __init__(self, username: str, password: str, id=None) -> None:
         self.username = username
-        self.__password = password
         self.__id = id
+
+        salt = bcrypt.gensalt()
+        self.__password = bcrypt.hashpw(password, salt)
+
+    def check_password(self, tried_password: str) -> bool:
+        return bcrypt.checkpw(tried_password, self.__password)
+
+    def create_zoning(self, description: str) -> Zoning:
+        ...
+
+    def get_calculations(self) -> list[DjuCalculation]:
+        ...
